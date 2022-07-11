fb6728965db34d1fdd2003b02e652031b83f6c86
Victor Souza <70328144+victorvssouza@users.noreply.github.com>
2022-07-11 17:39:47 -0300

    Add files via upload

--- Bhaskara Calculator/code.js
+++ Bhaskara Calculator/code.js
@@ -1,13 +1,19 @@
-var a;
-var b;
-var c;
+var a = 0;
+var b = 0;
+var c = 0;
 
 prompt("Insira o valor de a = ", a);
 prompt("Insira o valor de b = ", b);
 prompt("Insira o valor de c = ", c);
 
+// if (a = 0){
+//     alert("Variable a does not receive value 0")
+// }
+
 var delta = b * b - 4 * a * c;
 document.getElementById("delta").innerHTML = delta;
+
+if (delta <= -1 )
 
 var x1 = (-b + delta/delta) / 2*a
 document.getElementById("var_x1").innerHTML = x1;
